8c8d44d576ce80915ee15fdd71daa120abbfb588
Niklas Baudy <niklas.baudy@vanniktech.de>
2015-10-19 21:03:05 +0200

    Add Jacoco plugin

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -22,6 +22,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
   echo "Deploying snapshot..."
-  ./gradlew clean uploadArchives
+  ./gradlew clean build uploadArchives jacocoReport
   echo "Snapshot deployed!"
 fi
